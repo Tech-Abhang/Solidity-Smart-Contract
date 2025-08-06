6059b5ffda70982358f1e492cfc4760a72d37d60
Tech-Abhang <abhangpawar03@gmail.com>
2025-08-06 13:01:45 +0530

    calling contract from contract

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -2,4 +2,17 @@
 
 pragma solidity ^0.8.24;
 
-contract StorageFactory{}
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory{
+
+    //keep the name same as the constract to call contract
+    SimpleStorage[] public listOfSimpleStorage ;  //creating a varible for contract using contract
+
+    function createSimpleStorageContract() public {
+
+        SimpleStorage newSimpleStorage = new SimpleStorage();
+       listOfSimpleStorage.push(newSimpleStorage);  //saving the contract to the varible
+    }
+
+}
